35ee779d967e352539584650d28fb07c0dfb1418
LUK3-Projects <146751520+LUK3-Projects@users.noreply.github.com>
2023-10-31 03:19:52 -0400

    Updated login function Updated Venue/Service listing with phone numbers Added Call features to listings

--- Framework of Protoype/Framework_of_Protoype.py
+++ Framework of Protoype/Framework_of_Protoype.py
@@ -1,48 +1,56 @@
+import webbrowser
+import datetime
 import tkinter as tk
 from tkinter import ttk
+from tkinter.font import Font
 from PIL import Image, ImageTk
+
 
 # Define Venue Class
 class Venue:
-    def __init__(self, name, budget, location, max_capacity, image_path):
+    def __init__(self, name, budget, location, max_capacity, image_path, phone_number):
         self.name = name
         self.budget = budget
         self.location = location
         self.max_capacity = max_capacity
         self.image_path = image_path
+        self.phone_number = phone_number
 
-    # Define Services Class
+
+ # Define Services Class
 class Services:
-    def __init__(self, name, location, budget, service_type, image_path):
+    def __init__(self, name, location, budget, service_type, image_path, phone_number):
         self.name = name
         self.location = location
         self.budget = budget
         self.service_type = service_type
         self.image_path = image_path
+        self.phone_number = phone_number
 
 # Create a list of venues using the Venue class
 venues = [
-    Venue("Venue A", "$1000", "City A", 200, "venueA.jpg"),
-    Venue("Venue B", "$1500", "City B", 150, "venueB.jpg"),
-    Venue("Venue C", "$2000", "City C", 100, "venueC.jpg"),
+    Venue("Venue A", "$1000", "City A", 200, "venueA.jpg", "123-456-1111"),
+    Venue("Venue B", "$1500", "City B", 150, "venueB.jpg", "123-456-1112"),
+    Venue("Venue C", "$2000", "City C", 100, "venueC.jpg", "123-456-1113"),
+    Venue("Venue D", "$500", "City D", 50, "venueD.jpg", "123-456-1114"),
 ]
 
 # Create a list of services using the Services Class
 services = [
-    Services("Service 1", "Location 1", "$1000", "Type A", "service1.jpg"),
-    Services("Service 2", "Location 2", "$1500", "Type B", "service2.jpg"),
+    Services("Service 1", "Location 1", "$1000", "Type A", "service1.jpg", "123-456-1115"),
+    Services("Service 2", "Location 2", "$1500", "Type B", "service2.jpg", "123-456-1116"),
+    Services("Service 3", "Location 3", "$100", "Type C", "service3.jpg", "123-456-1117"),
+    Services("Service 4", "Location 4", "$500", "Type D", "service4.jpg", "123-456-1118"),
 ]
 
 # Create the main window
 root = tk.Tk()
 root.title("Venue Viewer")
+# Define chosen font from UI/UX team
+#custom_font = Font(family="X", size=20)
 
 # Create a Notebook widget for tabs
 notebook = ttk.Notebook(root)
-
-# Create Login Tab
-login_tab = ttk.Frame(notebook)
-notebook.add(login_tab, text='Login')
 
 # Creating user DB
 users = {
@@ -51,9 +59,23 @@ users = {
     # Add more users as needed
 }
 
+# Login page main function design start:
+
+# Define a global variable to keep track of login attempts
+login_attempts = 0
+locked_until = None
+
 # Define login function
 def login():
-    global current_user
+    global current_user, login_attempts, locked_until
+    
+    if locked_until and datetime.datetime.now() < locked_until:
+        remaining_time = (locked_until - datetime.datetime.now()).seconds
+        confirmation_label.config(text=f"", foreground='red')
+        return  # Do not proceed with login
+    else:
+        confirmation_label.config(text='')
+
     username = username_entry.get()
     password = password_entry.get()
 
@@ -64,21 +86,55 @@ def login():
         notebook.tab(1, state='normal')  # Enable Venues tab
         notebook.tab(2, state='normal')  # Enable Services tab
         notebook.tab(3, state='normal')  # Enable Search Venues tab
-        
+        credits_label.config(text='')  # Reset credits_label
+        login_attempts = 0  # Reset login attempts
     else:
         # Failed login
-        credits_label.config(text='Invalid credentials')
+        login_attempts += 1
+        if login_attempts >= 3:
+            # Lock login for 1 minute
+            locked_until = datetime.datetime.now() + datetime.timedelta(minutes=1)
+            login_attempts = 0  # Reset login attempts after locking
         confirmation_label.config(text='Invalid credentials', foreground='red')
+        update_timer()
+
+def update_timer():
+    global locked_until
+    if locked_until and datetime.datetime.now() < locked_until:
+        remaining_time = (locked_until - datetime.datetime.now()).seconds
+        timer_label.config(text=f"Too many invalid logins, please wait {remaining_time} seconds in order to try again.")
+        root.after(1000, update_timer)  # Update every second
+    else:
+        timer_label.config(text='')
+
+# Define Call function
+def initiate_call(phone_number):
+    # Use the webbrowser module to open the default VoIP application with the provided phone number
+    webbrowser.open(f"tel:{phone_number}")
 
 # Login page design START ---------------------------------------------------------------
 
+# Create Login Tab
+login_tab = ttk.Frame(notebook)
+notebook.add(login_tab, text='Login')
+
 # Load the CCLogo image
-cc_logo = Image.open("CCLogo.png")
+desired_width = 200 # Adjust desired width
+desired_height = 200  # Adjust desired height
+cc_logo = Image.open("CClogo.png")
+cc_logo = cc_logo.resize((desired_width, desired_height), Image.ANTIALIAS)
 cc_logo = ImageTk.PhotoImage(cc_logo)
-cc_logo_label = ttk.Label(login_tab, image=cc_logo)
-cc_logo_label.grid(row=0, column=0, columnspan=2, pady=(20, 0))
+
+# Place the logo in the login tab
+logo_label = ttk.Label(login_tab, image=cc_logo)
+logo_label.grid(row=0, column=0, columnspan=2, pady=(20, 0))
+
+# Assign the image to the label to prevent garbage collection
+logo_label.image = cc_logo
 
 # Username Entry
+# Example Code for font testing
+#username_label = ttk.Label(login_tab, text="Username:", font=custom_font)
 username_label = ttk.Label(login_tab, text="Username:")
 username_label.grid(row=1, column=0, padx=(50, 5), pady=(10, 5), sticky='e')
 username_entry = ttk.Entry(login_tab)
@@ -106,6 +162,15 @@ confirmation_label.grid(row=5, column=0, columnspan=2, pady=(0, 20))
 login_tab.grid_rowconfigure(6, weight=1)
 login_tab.grid_columnconfigure((0,1), weight=1)
 
+# binding return to enter
+username_entry.bind('<Return>', lambda event: login())
+password_entry.bind('<Return>', lambda event: login())
+
+# lockout timer
+timer_label = ttk.Label(login_tab, text='', font=('Arial', 12))
+timer_label.grid(row=6, column=0, columnspan=2, pady=(0, 20))
+
+
 # Login page design END ---------------------------------------------------------
 
 # Venue Tab design START --------------------------------------------------------
@@ -129,6 +194,11 @@ for i, venue in enumerate(venues):
     info = f"Name: {venue.name}\nBudget: {venue.budget}\nLocation: {venue.location}\nMax Capacity: {venue.max_capacity}"
     info_label = ttk.Label(venue_tab, text=info, justify='left')
     info_label.grid(row=i, column=1, padx=5, pady=5, sticky='w')
+
+    #testing
+    # call button venue
+    call_button = ttk.Button(venue_tab, text="Call", command=lambda number=venue.phone_number: initiate_call(number))
+    call_button.grid(row=i, column=2, padx=5, pady=5, sticky='w')
 
     label.image = image
 
@@ -154,6 +224,11 @@ for i, service in enumerate(services):
     info = f"Name: {service.name}\nLocation: {service.location}\nBudget: {service.budget}\nType: {service.service_type}"
     info_label = ttk.Label(services_tab, text=info, justify='left')
     info_label.grid(row=i, column=1, padx=5, pady=5, sticky='w')
+
+    #testing
+    #call button services
+    call_button = ttk.Button(services_tab, text="Call", command=lambda number=service.phone_number: initiate_call(number))
+    call_button.grid(row=i, column=2, padx=5, pady=5, sticky='w')
 
     label.image = image
 
@@ -238,6 +313,9 @@ def display_search_results(results):
         info = f"Name: {venue.name}\nBudget: {venue.budget}\nLocation: {venue.location}\nMax Capacity: {venue.max_capacity}"
         info_label = ttk.Label(search_results_frame, text=info, justify='left')
         info_label.grid(row=i, column=1, padx=5, pady=5, sticky='w')
+
+        call_button = ttk.Button(search_results_frame, text="Call", command=lambda number=venue.phone_number: initiate_call(number))
+        call_button.grid(row=i, column=2, padx=5, pady=5, sticky='w')
 
         label.image = image
 
@@ -334,9 +412,13 @@ def display_search_results_services(results):
         info_label = ttk.Label(search_results_frame_services, text=info, justify='left')
         info_label.grid(row=i, column=1, padx=5, pady=5, sticky='w')
 
+        call_button = ttk.Button(search_results_frame_services, text="Call", command=lambda number=service.phone_number: initiate_call(number))
+        call_button.grid(row=i, column=2, padx=5, pady=5, sticky='w')
+
         label.image = image
 
 # Search Tab Design END----------------------------------------------------------------------------------------------------------
+
 
 # Pack and start main loop
 notebook.pack(padx=10, pady=10)
